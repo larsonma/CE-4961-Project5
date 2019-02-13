4f8a9f12bb855c03f18c2f4a5914b0b413148ea7
larsonma <larsonma@msoe.edu>
2019-02-13 03:44:55 +0000

    Refactored identity to block

--- identity.py
+++ identity.py
@@ -1,6 +1,7 @@
 import socket
 
 DEVICE_TYPE = 'lamp'
+BUFFER_SIZE = 1024
 
 def getName():
     return socket.gethostname()
@@ -10,3 +11,14 @@ def getDeviceType():
 
 def getIdentity():
     return 'name: {0}\r\ntype: {1}\r\n'.format(getName(), getDeviceType())
+
+def identify(sock):
+    while True:
+        message, address = sock.recvfrom(BUFFER_SIZE)
+        message = message.decode('ascii')
+
+        if message == 'operation: discover\r\n':
+            dev_id = getIdentity().encode('ascii')
+            sock.sendto(dev_id, address)
+
+            return address
